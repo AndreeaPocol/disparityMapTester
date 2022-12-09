b9bdf705bd9cb54c4564e3513f0893e035fd7224
Andreea Pocol <andreeapocol@ymail.com>
2022-12-08 19:12:07 -0500

    added more imaegs and fixed syntax issues

--- testsuite.sh
+++ testsuite.sh
@@ -6,12 +6,30 @@
 # Run with `report` to run on all test images and
 # provide a specific image name to run once. 
 
-clear
 
 images=(
-    teddy
-    baby
     aloe
+    art
+    # baby
+    # bowling1
+    # bowling2
+    # lines
+    # teddy
+    # box
+    # cat
+    # cup
+    # cups
+    # globe1
+    # globe2
+    # grapes
+    # lilies
+    # peaches
+    # reflections
+    # rocks
+    # roses
+    # sail
+    # sphere
+    # woods
 )
 
 generators=(
@@ -30,15 +48,21 @@ if [ $# -ge 1 ] ; then
         mkdir report
         for i in "${images[@]}"; do
             for j in "${generators[@]}"; do
-                python3 testDisparityMap.py inputs/$i/$j/left_disparity_$i.png inputs/$i/$j/right_disparity_$i.png inputs/$i/$i_L.png inputs/$i/$i_R.png report/left_disparity_$i_$j_score.png
+                python3 testDisparityMap.py inputs/$i/$j/left_disparity_${i}.png inputs/$i/$j/right_disparity_${i}.png inputs/$i/${i}_L.png inputs/$i/${i}_R.png report/left_disparity_${i}_${j}_score.png
             done
+        done
+    elif [ $1 == "maps" ] ; then
+        echo "Generating disparity maps"
+        mkdir $1
+        for i in "${images[@]}"; do
+            python3 generateDisparityMaps.py inputs/$i/${i}_L.png inputs/$i/${i}_R.png inputs/$i/opencv1/left_disparity_${i}.png inputs/$i/opencv1/right_disparity_${i}.png
         done
     else
         echo "Running single test and writing output to test_data..."
         mkdir $1
-        for j in "${generators[@]}"; do
-            python3 testDisparityMap.py inputs/$1/$j/left_disparity_$1.png inputs/$1/$j/right_disparity_$1.png inputs/$1/$1_L.png inputs/$1/$1_R.png $1/left_disparity_$1_${j}_score.png
-            python3 computeDisparityMapScore.py $1/left_disparity_$1_${j}_score.png
+        for g in "${generators[@]}"; do
+            python3 testDisparityMap.py inputs/$1/$g/left_disparity_$1.png inputs/$1/$g/right_disparity_$1.png inputs/$1/$1_L.png inputs/$1/$1_R.png $1/left_disparity_$1_${g}_score.png
+            python3 computeDisparityMapScore.py $1/left_disparity_$1_${g}_score.png
         done
     fi
 fi
